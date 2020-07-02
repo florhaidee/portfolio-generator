9fe38b43aa876895b4188e3a5712d9c6a4549c9a
Florhaidee <florhaideedev@gmail.com>
2020-07-01 22:17:11 -0700

    printed dynamic output genrated webpages

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+/* -----  how to read nd print from command line --------*/
+/*const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
-
 const printProfileData = (profileDataArr) => {
     profileDataArr.forEach(profileItem => console.log(profileItem));
 };
+printProfileData(profileDataArgs);*/
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2, process.argv.length); // variable that holds the user command-line arguments
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
   
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});

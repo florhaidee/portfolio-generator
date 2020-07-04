1f2f5808b8e66c433122f216434c2a3188f98452
Florhaidee <florhaideedev@gmail.com>
2020-07-04 00:56:32 -0600

    create dynamically the page-template

--- app.js
+++ app.js
@@ -28,7 +28,8 @@
 //     }
 //   ])
 //   .then(answers => console.log(answers));
-
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const inquirer = require('inquirer');
 /* ---- Profile questions --------*/
 const promptUser = () => {
@@ -155,5 +156,11 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-});
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
+}); 
